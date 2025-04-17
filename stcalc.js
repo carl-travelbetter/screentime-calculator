a07706f77192c9df0a0d381ad220c18963cf4bc4
carl-travelbetter <cmciver@travelbetter.co.uk>
2025-04-17 15:18:10 +0100

    Add files via upload

--- stcalc.js
+++ stcalc.js
@@ -15,6 +15,7 @@ let lifeTimeDays = 0;
 let lifeTimeYears = 0;
 let restOfYourLifeScreenTime = 0;
 let ageSubmitted = "N";
+let dailyScreenTimeMinutes = 0;
 
 function calculateScreenTime()
 {
@@ -23,10 +24,12 @@ function calculateScreenTime()
     const hrs = document.getElementById("hrs").value;
     const mins = document.getElementById("mins").value;
     const age = document.getElementById("age").value;
+    dailyScreenTimeMinutes = (hrs*60) + +mins;
     console.log("Hrs = "+hrs+" Mins = "+mins);
-    calculateSevenDays(hrs, mins);
-    calculateMonth(hrs, mins);
-    calculateYear(hrs, mins);
+    console.log("Daily Screen Time Minutes ="+dailyScreenTimeMinutes);
+    calculateSevenDays(dailyScreenTimeMinutes);
+    calculateMonth(dailyScreenTimeMinutes);
+    calculateYear(dailyScreenTimeMinutes);
     calculateLifetime();
     if (age > 0)
     {
@@ -47,7 +50,15 @@ function calculateScreenTime()
 function calculateHowMuchLeft(age)
 {
     console.log("How Much Left");
-    let yearsLeft = 80 - age;
+    let yourAge = +age + 0;
+    let yearsLeft = 100;
+    if (yourAge < 80)
+    {
+        yearsLeft = 80 - age;   
+    }
+    else {
+        yearsLeft = 10;
+    }
     restOfYourLifeScreenTime = ((yearsLeft*daysPerYear) / 365).toFixed(2);
 }
 
@@ -62,33 +73,33 @@ function calculateLifetime()
     
 }
 
-function calculateYear(hrs, mins)
+function calculateYear(dailyMinutes)
 {
     console.log("C365D: Calculate 365 Days (1 Year)...");
-    console.log("C365D: hrs "+hrs+" mins"+mins);
-    hrsYearCalc = +hrs*365;
-    minsYearCalc = +mins*365;
-    console.log("C365D: year hrs "+hrsYearCalc+" mins "+minsYearCalc);
+    //console.log("C365D: hrs "+hrs+" mins"+mins);
+    //hrsYearCalc = +hrs*365;
+    minsYearCalc = +dailyMinutes*365;
+    console.log("C365D: mins "+minsYearCalc);
     let yearMinsToHours = parseInt(minsYearCalc / 60);
     let takeAwayMins = 0;
     if (yearMinsToHours < 1)
     {
         console.log("C365D: yearly mins less than 1 hour");
-        minsPerYear = +yearMins;
-        hrsPerYear = +yearHrs;
+        minsPerYear = +minsYearCalc;
+        hrsPerYear = +yearMinsToHours;
     }
     else
     {
         takeAwayMins = +yearMinsToHours * 60;
         minPerYear = +minsYearCalc - +takeAwayMins;
-        hrsPerYear = +hrsYearCalc + +yearMinsToHours;
+        hrsPerYear = +yearMinsToHours;
         console.log("TakeAwayMins "+takeAwayMins);
         console.log("MinResultYear "+minPerYear);
         console.log("HrsResultsYear "+hrsPerYear);
     }
     console.log("C365D: Results "+hrsPerYear+"hrs "+minPerYear+"mins");
-    let totalMinutes = (hrsYearCalc * 60)+ +minsYearCalc;
-    let days = calcDays(totalMinutes);
+    //let totalMinutes = (hrsYearCalc * 60)+ +minsYearCalc;
+    let days = calcDays(minsYearCalc);
     //set Year Days for LifeTime calcDays
     daysPerYear = days;
     console.log("C365D: Days "+days);
@@ -96,47 +107,44 @@ function calculateYear(hrs, mins)
     
 }
 
-function calculateMonth(hrs, mins)
+function calculateMonth(dailyMinutes)
 {
     console.log("C30D: Calculate 30 days (1 Month)... ");
-    console.log("C30D: hrs "+hrs+" mins "+mins);
-    let monthHrs = +hrs*30;
-    let monthMins = +mins*30;
-    console.log("C30D: month hrs "+monthHrs+" mins "+monthMins);
+   // console.log("C30D: hrs "+hrs+" mins "+mins);
+   // let monthHrs = +hrs*30;
+    let monthMins = +dailyMinutes*30;
+    console.log("C30D: month mins "+monthMins);
     let monthMinsToHours = parseInt(monthMins / 60);
     let takeAwayMins = 0;
     if (monthMinsToHours < 1)
     {
         console.log("C30D: monthly mins less than 1 hour");
         minsPerMonth = +monthMins;
-        hrsPerMonth = +monthHrs;
+        hrsPerMonth = +monthMinsToHours;
     }
     else
     {
         takeAwayMins = monthMinsToHours * 60;
         minsPerMonth = +monthMins - +takeAwayMins;
-        hrsPerMonth = +monthHrs + +monthMinsToHours;
+        hrsPerMonth = +monthMinsToHours;
         console.log("TakeAwayMins "+takeAwayMins);
         console.log("MinResultMonth "+minsPerMonth);
         console.log("HrsResultsMonth "+hrsPerMonth);
     }
     console.log("C30D: Results "+hrsPerMonth+"hrs "+minsPerMonth+"mins");
-    let totalMins = (monthHrs*60)+ +monthMins;
-    const days = calcDays(totalMins);
+    //let totalMins = (monthHrs*60)+ +monthMins;
+    const days = calcDays(monthMins);
     console.log("C30D: Results in Day "+days);
     daysPerMonth = days;
     
-   
     
 }
 
-function calculateSevenDays(hrs, mins)
+function calculateSevenDays(dailyMinutes)
 { 
     console.log("Calculate Seven Days...");
-    console.log("C7D: hrs "+hrs+" mins "+mins);
-    let weekhrs = +hrs*7;
-    let weekmins = +mins*7;
-    console.log("C7D: weekhr "+weekhrs+" weekmins "+weekmins);
+    let weekmins = +dailyMinutes*7;
+    console.log("C7D: weekmins "+weekmins);
     let weekMinsToHours = parseInt(weekmins / 60);
     console.log("C7D: Hours in Week Mins Ratio "+weekMinsToHours);
     let takeAwayMins = 0;
@@ -144,31 +152,25 @@ function calculateSevenDays(hrs, mins)
     {
         console.log("C7D: Week Mins to Hours less than 1 hour");
         minsPerWeek = +weekmins;
-        hrsPerWeek = +weekhrs;
+        hrsPerWeek = +weekMinsToHours;
     }
     else 
     {
         takeAwayMins = weekMinsToHours * 60;
         minsPerWeek = +weekmins - +takeAwayMins;
-        hrsPerWeek = +weekhrs + +weekMinsToHours;
+        hrsPerWeek = +weekMinsToHours;
         console.log("TakeAwayMins "+takeAwayMins);
         console.log("MinResultWeel "+minsPerWeek);
         console.log("HrsResultsWeek "+hrsPerWeek);
         
     }
-    
+   
     console.log("C7D: Result for a week: Hrs "+hrsPerWeek+" mins "+minsPerWeek);
-    
-    
-    
-    
-    
     let totalMinutes = (hrsPerWeek*60)+minsPerWeek;
     let days = calcDays(totalMinutes);
     daysPerWeek = days;
     console.log("C7D: Total Minutes "+totalMinutes);
-    
-    
+  
 }
 
 function calcDays(totalMinutes) 
